e5bf4b966e9bc92e2df9d5789bf3ac0290a4a7ff
Maria <mariabrodowska89@gmail.com>
2025-09-12 21:09:43 +0200

    feat: Update user preferences storage, implement POST and DELETE /preferences/:userId

--- src/controllers/preferences.controller.ts
+++ src/controllers/preferences.controller.ts
@@ -3,7 +3,7 @@ import { UserPreferences } from "src/types/preferences";
 
 const preferences = new Map<string, UserPreferences>();
 
-preferences.set("user", {
+preferences.set("user1", {
   dnd: { start: "22:00", end: "07:00" },
   eventSettings: {
     item_shipped: { enabled: true },
@@ -11,10 +11,16 @@ preferences.set("user", {
   },
 });
 
+preferences.set("user2", {
+  dnd: { start: "21:00", end: "06:00" },
+  eventSettings: {
+    item_shipped: { enabled: true },
+    invoice_generated: { enabled: false },
+  },
+});
+
 export const preferencesControllerFactory = (app: Express) => {
   app.get("/preferences/:userId", (req, res) => {
-    // console.log(`Preferences for user ${req.params.userId} requested`);
-
     const userId = req.params.userId;
     const userPrefs = preferences.get(userId);
 
@@ -23,12 +29,42 @@ export const preferencesControllerFactory = (app: Express) => {
         .status(404)
         .send({ message: "No preferences found for this user" });
 
+    console.log(`Preferences for user ${req.params.userId} requested`);
     res.status(200).send(userPrefs);
   });
 
   app.post("/preferences/:userId", (req, res) => {
-    console.log(`Preferences for user ${req.params.userId} updated`, req.body);
+    const userId = req.params.userId;
+    const userPrefs: UserPreferences = req.body;
 
-    res.status(200).send({ message: "Hello!" });
+    if (!userPrefs || Object.keys(userPrefs).length === 0)
+      return res.status(400).send({ message: "Preferences data is required" });
+
+    if (!userPrefs.eventSettings)
+      userPrefs.eventSettings = {
+        item_shipped: { enabled: true },
+        invoice_generated: { enabled: true },
+      };
+
+    preferences.set(userId, userPrefs);
+
+    console.log(`Preferences for user ${req.params.userId} updated`, req.body);
+    res
+      .status(200)
+      .send({ message: "Preferences saved", preferences: userPrefs });
+  });
+
+  app.delete("/preferences/:userId", (req, res) => {
+    const userId = req.params.userId;
+
+    if (!preferences.has(userId))
+      return res
+        .status(404)
+        .send({ message: "No preferences found for this user" });
+
+    preferences.delete(userId);
+
+    console.log(`Preferences deleted for user ${req.params.userId}`);
+    res.status(200).send({ message: "Preferences deleted" });
   });
 };
