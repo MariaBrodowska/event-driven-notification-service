6a511cd819fe707ce7e9b072ec35ffed287acdab
Maria <mariabrodowska89@gmail.com>
2025-09-12 20:20:35 +0200

    feat: Add storage for user preferences and implement GET /preferences/:userId

--- src/controllers/preferences.controller.ts
+++ src/controllers/preferences.controller.ts
@@ -1,15 +1,34 @@
-import { Express } from 'express';
+import { Express } from "express";
+import { UserPreferences } from "src/types/preferences";
+
+const preferences = new Map<string, UserPreferences>();
+
+preferences.set("user", {
+  dnd: { start: "22:00", end: "07:00" },
+  eventSettings: {
+    item_shipped: { enabled: true },
+    invoice_generated: { enabled: true },
+  },
+});
 
 export const preferencesControllerFactory = (app: Express) => {
-	app.get('/preferences/:userId', (req, res) => {
-		console.log(`Preferences for user ${req.params.userId} requested`);
+  app.get("/preferences/:userId", (req, res) => {
+    // console.log(`Preferences for user ${req.params.userId} requested`);
 
-		res.status(200).send({ message: 'Hello!' });
-	});
+    const userId = req.params.userId;
+    const userPrefs = preferences.get(userId);
 
-	app.post('/preferences/:userId', (req, res) => {
-		console.log(`Preferences for user ${req.params.userId} updated`, req.body);
+    if (!userPrefs)
+      return res
+        .status(404)
+        .send({ message: "No preferences found for this user" });
 
-		res.status(200).send({ message: 'Hello!' });
-	});
+    res.status(200).send(userPrefs);
+  });
+
+  app.post("/preferences/:userId", (req, res) => {
+    console.log(`Preferences for user ${req.params.userId} updated`, req.body);
+
+    res.status(200).send({ message: "Hello!" });
+  });
 };
---
+++ src/types/preferences.ts
@@ -0,0 +1,13 @@
+export interface EventSetting {
+  enabled: boolean;
+}
+
+export interface DND {
+  start: string;
+  end: string;
+}
+
+export interface UserPreferences {
+  dnd?: DND;
+  eventSettings?: Record<string, EventSetting>;
+}
